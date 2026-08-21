d2f005aa91cb3046c574326dcbce76d6f5ab7485
Godfrey <charambagodfrey@gmail.com>
2026-08-21 17:24:23 +0200

    render changes

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import authService from './users_service';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://smarthire-backend-67oy.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
